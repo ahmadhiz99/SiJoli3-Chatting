062681b0830450e28e82eff64bbf62caac436add
GiziNusantara <ahmadhizbullahakbar@gmail.com>
2020-03-23 09:18:29 +0700

    second commit

--- app/src/main/java/com/SiJoLi/SiJoLi/SplashScreen.java
+++ app/src/main/java/com/SiJoLi/SiJoLi/SplashScreen.java
@@ -2,13 +2,28 @@ package com.SiJoLi.SiJoLi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    //4000=4 detik
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i =new Intent(SplashScreen.this, LoginMenu.class);
+                startActivity(i);
+                finish();
+
+            }
+        },waktu_loading);
     }
 }
